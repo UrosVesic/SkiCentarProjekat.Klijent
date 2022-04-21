af5a307e11b9af08d026f80e301d98ec8560d78e
UrosVesic <uros99uki@gmail.com>
2022-04-21 15:23:48 +0200

    Dodate glavna forma, forma za prijavu i apstraktna klasa OpstaEktranskaForma

---
+++ src/main/java/rs/ac/bg/fon/np/sc/klijent/forme/OpstaEkranskaForma.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.fon.np.sc.klijent.forme;
+
+import rs.ac.bg.fon.np.sc.commonLib.domen.OpstiDomenskiObjekat;
+
+
+/**
+ *
+ * @author UrosVesic
+ */
+public abstract class OpstaEkranskaForma extends javax.swing.JFrame {
+
+    public abstract OpstiDomenskiObjekat kreirajObjekat();
+
+}
